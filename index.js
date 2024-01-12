9708f6590cbb31e09a5355efe2611916dd683565
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-12 13:38:55 +0530

    keyPressSound Added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-
 let txt = document.querySelector(".text");
 let box = document.querySelector(".box");
+let audio = new Audio("analog-appliance-button-2-185277.mp3");
 
 document.addEventListener("keydown", (e) => {
   box.classList.add("active");
-
+  audio.play();
   let keyName = e.keyCode === 32 ? "space" : e.key;
   box.querySelector(".keyCode").innerText = e.keyCode;
   box.querySelector(".keyName").innerText = keyName;
@@ -14,5 +13,7 @@ document.addEventListener("keydown", (e) => {
   box.querySelector(".code span").innerText = e.keyCode;
 
   txt.innerText = `Pressed key`;
-//   console.log(e);
+  //   console.log(e);
 });
+
+
